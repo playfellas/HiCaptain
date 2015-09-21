5cc613000caea99027a2133c3ee38a4387b8ccc9
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-09-21 14:52:44 +0200

    First impl

--- app/src/main/java/it/playfellas/hicaptain/MainActivity.java
+++ app/src/main/java/it/playfellas/hicaptain/MainActivity.java
@@ -1,13 +1,58 @@
 package it.playfellas.hicaptain;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends ImmersiveAppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Bind(R.id.centralImageView)
+    ImageButton centralImageView;
+    @Bind(R.id.captainImageView)
+    ImageButton captainImageView;
+    @Bind(R.id.pruaImageView)
+    ImageButton pruaImageView;
+
+    private boolean captainEnabled = true;
+    private boolean pruaEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.captainImageView)
+    public void captainClicked(View v) {
+        if(captainEnabled && !pruaEnabled) {
+            Log.d(TAG, "Captain clicked");
+        }
+
+        captainEnabled = false;
+        pruaEnabled = true;
+    }
+
+    @OnClick(R.id.pruaImageView)
+    public void pruaClicked(View v) {
+        if(!captainEnabled && pruaEnabled) {
+            Log.d(TAG, "Prua clicked");
+        }
+
+        captainEnabled = false;
+        pruaEnabled = false;
     }
 }
